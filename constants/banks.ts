5c1e77afe63350601ef32cf5610ce82dced2a41e
Tomisin Leshi <tom-lesh@outlook.com>
2024-09-24 11:46:48 +0100

    Constant updated

--- constants/banks.ts
+++ constants/banks.ts
@@ -12,3 +12,575 @@ export const Banks = [
     name: "Ecobank Nigeria",
   },
 ];
+
+// LIST OF BANKS FOR EACH CURRENCY(THOSE WE ARE USING NOW AND LATER)
+export const AllCurrencyBanks = [
+  {
+    currency: "NGN",
+    banks: [
+      {
+        label: "9Payment Service Bank",
+        name: "9Payment service bank",
+        value: "120001",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080001/LINK/Bank_List/9bank_c0jqji.png",
+      },
+      {
+        label: "Access Bank",
+        name: "Access bank",
+        value: "000014",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739938/LINK/Bank_List/accessbank_w23utv.png",
+      },
+      {
+        label: "Access Bank (Diamond)",
+        name: "Access bank (diamond)",
+        value: "000005",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739938/LINK/Bank_List/accessbank_w23utv.png",
+      },
+      {
+        label: "CIT Microfinance Bank",
+        name: "CIT Microfinance Bank",
+        value: "090144",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739938/LINK/Bank_List/citbank_ey1a4x.png",
+      },
+      {
+        label: "Citibank Nigeria",
+        name: "Citibank nigeria",
+        value: "000009",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739938/LINK/Bank_List/Citibank_zozj7m.png",
+      },
+      {
+        label: "Ecobank",
+        name: "Ecobank",
+        value: "000010",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080001/LINK/Bank_List/logo-ecobank_otqxdd.jpg",
+      },
+      {
+        label: "Eyowo",
+        name: "Eyowo",
+        value: "090328",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080001/LINK/Bank_List/eyowo_bank_ha0alm.png",
+      },
+      {
+        label: "Fidelity Bank",
+        name: "Fidelity bank",
+        value: "000007",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739938/LINK/Bank_List/fidelitybank_now8zu.png",
+      },
+      {
+        label: "First Bank of Nigeria",
+        name: "First bank of nigeria",
+        value: "000016",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739938/LINK/Bank_List/firstbank_edtzmk.png",
+      },
+      {
+        label: "Globus Bank",
+        name: "Globus bank",
+        value: "000027",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080001/LINK/Bank_List/globus_bank_s7lnyj.png",
+      },
+      {
+        label: "Guaranty Trust Bank",
+        name: "Guaranty trust bank",
+        value: "000013",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739938/LINK/Bank_List/gtbank_s1exin.png",
+      },
+      {
+        label: "Heritage Bank",
+        name: "Heritage bank",
+        value: "000020",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080001/LINK/Bank_List/heritage_bank_okc5jk.png",
+      },
+      {
+        label: "Jaiz Bank",
+        name: "Jaiz bank",
+        value: "000006",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080001/LINK/Bank_List/jaiz_bank_azpxpv.jpg",
+      },
+      {
+        label: "Keystone Bank",
+        name: "Keystone bank",
+        value: "000002",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080001/LINK/Bank_List/keystone_xekrxb.png",
+      },
+      {
+        label: "Kuda",
+        name: "Kuda",
+        value: "090267",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739938/LINK/Bank_List/kudabank_zfrwks.png",
+      },
+      {
+        label: "Moniepoint Bank",
+        name: "Moniepoint bank",
+        value: "090405",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1706363918/LINK/Bank_List/var-1_utohuh.svg",
+      },
+      {
+        label: "OPAY",
+        name: "Opay",
+        value: "100004",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080001/LINK/Bank_List/opay_yp9g6x.png",
+      },
+      {
+        label: "Paga",
+        name: "Paga",
+        value: "100002",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739938/LINK/Bank_List/pagabank_jnkrck.png",
+      },
+      {
+        label: "PalmPay",
+        name: "Palmpay",
+        value: "100033",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739938/LINK/Bank_List/palmpaybank_ayyotp.png",
+      },
+
+      {
+        label: "Parallex Bank",
+        name: "Parallex bank",
+        value: "000030",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080329/LINK/Bank_List/parallex_uhjga5.png",
+      },
+      {
+        label: "Polaris Bank",
+        name: "Polaris bank",
+        value: "000008",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080329/LINK/Bank_List/polaris_sxds2t.png",
+      },
+      {
+        label: "Providus Bank",
+        name: "Providus bank",
+        value: "000023",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739938/LINK/Bank_List/providusbank_rgh1wt.png",
+      },
+      {
+        label: "Stanbic IBTC Bank",
+        name: "Stanbic ibtc bank",
+        value: "000012",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739939/LINK/Bank_List/stanbicbank_kujbym.png",
+      },
+      {
+        label: "Standard Chartered Bank",
+        name: "Standard chartered bank",
+        value: "000021",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739939/LINK/Bank_List/standardbank_jgmmgj.png",
+      },
+      {
+        label: "Sterling Bank",
+        name: "Sterling bank",
+        value: "000001",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739939/LINK/Bank_List/sterlingbank_f8o7xw.png",
+      },
+      {
+        label: "Suntrust Bank",
+        name: "Suntrust bank",
+        value: "000022",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080330/LINK/Bank_List/suntrust_vfl4zn.png",
+      },
+      {
+        label: "TITAN TRUST BANK",
+        name: "Titan trust bank",
+        value: "000025",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1703080329/LINK/Bank_List/titan_o7drak.png",
+      },
+      {
+        label: "Union Bank of Nigeria",
+        name: "Union bank of nigeria",
+        value: "000018",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739939/LINK/Bank_List/unionbank_os7lys.png",
+      },
+      {
+        label: "United Bank for Africa",
+        name: "United bank for africa",
+        value: "000004",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739939/LINK/Bank_List/ubabank_kgvyvn.png",
+      },
+      {
+        label: "Unity Bank",
+        name: "Unity bank",
+        value: "000011",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739939/LINK/Bank_List/unitybank_t9ystt.png",
+      },
+      {
+        label: "VFD  Bank",
+        name: "VFD microfinance bank",
+        value: "090110",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739939/LINK/Bank_List/vfdbank_qu8vt0.png",
+      },
+      {
+        label: "Wema Bank",
+        name: "Wema bank",
+        value: "000017",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739939/LINK/Bank_List/wemabank_wj9npl.png",
+      },
+      {
+        label: "Zenith Bank",
+        name: "Zenith bank",
+        value: "000015",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1675739939/LINK/Bank_List/zenithbank_qx59ic.png",
+      },
+    ],
+  },
+  {
+    currency: "USD",
+    banks: [
+      {
+        name: "Chase Bank",
+        label: "Chase Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730129/LINK/Bank_List/USD/Chase_Bank_tzhpb8.svg",
+      },
+      {
+        name: "Bank of America",
+        label: "Bank of America",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730129/LINK/Bank_List/USD/Bank_of_America_omy6lx.svg",
+      },
+      {
+        name: "Wells Fargo Bank",
+        label: "Wells Fargo Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730125/LINK/Bank_List/USD/Wells_Fargo-Logo.wine_dxwfjq.svg",
+      },
+      {
+        name: "Citibank",
+        label: "Citibank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730128/LINK/Bank_List/USD/Citigroup-Logo.wine_nmdliz.svg",
+      },
+      {
+        name: "U.S. Bank",
+        label: "U.S. Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730125/LINK/Bank_List/USD/US_Bank_h2o3ic.svg",
+      },
+      {
+        name: "Truist Bank",
+        label: "Truist Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730125/LINK/Bank_List/USD/Truist_Bank_ncvvdc.svg",
+      },
+      {
+        name: "PNC Bank",
+        label: "PNC Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730127/LINK/Bank_List/USD/PNC_bank_r0i4lr.svg",
+      },
+      {
+        name: "TD Bank",
+        label: "TD Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730125/LINK/Bank_List/USD/TD_Bank_ueqqhn.svg",
+      },
+      {
+        name: "Capital One",
+        label: "Capital One",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730129/LINK/Bank_List/USD/Capital_One_Bank_w4alew.svg",
+      },
+      {
+        name: "HSBC Bank USA",
+        label: "HSBC Bank USA",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730127/LINK/Bank_List/USD/HSBC_Bank_bh95aw.svg",
+      },
+      {
+        name: "Charles Schwab Bank",
+        label: "Charles Schwab Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730129/LINK/Bank_List/USD/Charles_Schwab_Corporation-Logo.wine_cc8aet.svg",
+      },
+      {
+        name: "Ally Bank",
+        label: "Ally Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730129/LINK/Bank_List/USD/Ally_Financial-Logo.wine_rkajxm.svg",
+      },
+      {
+        name: "USAA Federal Savings Bank",
+        label: "USAA Federal Savings Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730125/LINK/Bank_List/USD/USAA_tvdeas.svg",
+      },
+      {
+        name: "Fifth Third Bank",
+        label: "Fifth Third Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730127/LINK/Bank_List/USD/Fifth_Third_Bank_xjmcsp.svg",
+      },
+      {
+        name: "KeyBank",
+        label: "KeyBank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730127/LINK/Bank_List/USD/KeyBank_jst5qp.svg",
+      },
+      {
+        name: "Citizens Bank",
+        label: "Citizens Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730127/LINK/Bank_List/USD/Citizens_Bank_po5xkv.svg",
+      },
+      {
+        name: "Regions Bank",
+        label: "Regions Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730127/LINK/Bank_List/USD/Regions_Bank_ucixe3.svg",
+      },
+      {
+        name: "Santander Bank",
+        label: "Santander Bank",
+        image:
+          "https://res.cloudinary.com/dqw0lwkil/image/upload/v1714730125/LINK/Bank_List/USD/Santander_Bank_dembwm.svg",
+      },
+    ],
+  },
+];
+
+// LIST OF FIAT CURRENCIES (THOSE WE ARE USING NOW AND LATER)
+export const fiatCurrency = [
+  {
+    href: "/",
+    key: "ars",
+    label: "ars",
+    currency: "ARS",
+    flag: "/assets/svg/fiat/ars.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "aud",
+    label: "aud",
+    currency: "AUD",
+    flag: "/assets/svg/fiat/aud.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "brl",
+    label: "brl",
+    currency: "BRL",
+    flag: "/assets/svg/fiat/brl.svg",
+  },
+  {
+    href: "/",
+    key: "cad",
+    label: "cad",
+    currency: "CAD",
+    flag: "/assets/svg/fiat/cad.svg",
+  },
+  {
+    href: "/",
+    key: "chf",
+    label: "chf",
+    currency: "CHF",
+    flag: "/assets/svg/fiat/chf.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "eur",
+    label: "eur",
+    currency: "EUR",
+    flag: "/assets/svg/fiat/eur.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "gbp",
+    label: "gbp",
+    currency: "GBP",
+    flag: "/assets/svg/fiat/gbp.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "ghs",
+    label: "ghs",
+    currency: "GHS",
+    flag: "/assets/svg/fiat/ghs.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "idr",
+    label: "idr",
+    currency: "IDR",
+    flag: "/assets/svg/fiat/idr.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "isk",
+    label: "isk",
+    currency: "ISK",
+    flag: "/assets/svg/fiat/isk.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "jpy",
+    label: "jpy",
+    currency: "JPY",
+    flag: "/assets/svg/fiat/jpy.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "kes",
+    label: "kes",
+    currency: "KES",
+    flag: "/assets/svg/fiat/kes.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "ngn",
+    label: "ngn",
+    currency: "NGN",
+    flag: "/assets/svg/fiat/ngn.svg",
+  },
+  {
+    href: "/",
+    key: "nzd",
+    label: "nzd",
+    currency: "NZD",
+    flag: "/assets/svg/fiat/nzd.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "sgd",
+    label: "sgd",
+    currency: "SGD",
+    flag: "/assets/svg/fiat/sgd.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "try",
+    label: "try",
+    currency: "TRY",
+    flag: "/assets/svg/fiat/try.svg",
+    status: "unavailable",
+  },
+  {
+    href: "/",
+    key: "usd",
+    label: "usd",
+    currency: "USD",
+    flag: "/assets/svg/fiat/usd.svg",
+  },
+  {
+    href: "/",
+    key: "zar",
+    label: "zar",
+    currency: "ZAR",
+    flag: "/assets/svg/fiat/zar.svg",
+    status: "unavailable",
+  },
+];
+
+// LIST OF VENDOR DETAILS FOR EACH CURRENCY(THOSE WE ARE USING NOW AND LATER)
+export const AllVendorList = [
+  {
+    currency: "BRL",
+    banks: [
+      {
+        PixKey: "11754090-d0cf-4b1b-9d45-6669b695f5f3",
+      },
+    ],
+  },
+  // {
+  //   currency: "IDR",
+  //   banks: [
+  //     {
+  //       venderName: "Rupiah Token ke LINK Technology Limited",
+  //       vendorNumber: "1236260109020137",
+  //       vendorBank: "Bank Rakyat Indonesia (BRI)",
+  //     },
+  //     {
+  //       venderName: "Rupiah Token ke LINK Technology Limited",
+  //       vendorNumber: "8889940109020137",
+  //       vendorBank: "Bank Mandiri",
+  //     },
+  //   ],
+  // },
+  {
+    currency: "NGN",
+    banks: [
+      {
+        venderName: "PASSPOINT (Okemdinachi Nwaejie)",
+        vendorNumber: "9636369057",
+        vendorBank: "Providus Bank",
+      },
+      {
+        venderName: "PASSPOINT (christopher okoye)",
+        vendorNumber: "9631525849",
+        vendorBank: "Providus Bank",
+      },
+      {
+        venderName: "PASSPOINT (marvel ekpo)",
+        vendorNumber: " 9628442937",
+        vendorBank: "Providus Bank",
+      },
+      {
+        venderName: "PASSPOINT (divine-will ekpo)",
+        vendorNumber: "9637681398",
+        vendorBank: "Providus Bank",
+      },
+      {
+        venderName: "PASSPOINT (emmanuel evarist)",
+        vendorNumber: "9618182180",
+        vendorBank: "Providus Bank",
+      },
+      {
+        venderName: "PASSPOINT (victor onyezia)",
+        vendorNumber: "9611298145",
+        vendorBank: "Providus Bank",
+      },
+    ],
+  },
+  {
+    currency: "USD",
+    banks: [
+      {
+        venderName: "LINK FINANCIAL INC",
+        vendorNumber: "202455624518",
+        vendorBank: "Choice Financial Group",
+        routingNumber: "091311229",
+        accountType: "Checking",
+      },
+    ],
+  },
+];
