d2f09f7db42899ca1aa326a72317c0aa754ed587
Tomisin Leshi <tom-lesh@outlook.com>
2025-04-30 15:42:20 +0100

    Update token.ts

--- constants/token.ts
+++ constants/token.ts
@@ -98,13 +98,6 @@ export const Tokens = [
         status: "available",
       },
       // {
-      //   name: "EUROE",
-      //   mintAddress: "2VhjJ9WxaGC3EZFwJG9BDUs9KxKCAjQY4vgd1qxgYWVg",
-      //   decimals: 6,
-      //   image: "/assets/svg/stablecoins/euroe.svg",
-      //   status: "available",
-      // },
-      // {
       //   name: "GYEN",
       //   mintAddress: "Crn4x1Y2HUKko7ox2EZMT6N2t2ZyH7eKtwkBGVnhEq1g",
       //   decimals: 6,
@@ -119,26 +112,19 @@ export const Tokens = [
       //   status: "available",
       // },
       // {
-      //   name: "ISKT",
-      //   mintAddress: "isktkk27QaTpoRUhwwS5n9YUoYf8ydCuoTz5R2tFEKu",
-      //   decimals: 6,
-      //   image: "/assets/svg/stablecoins/iskt.svg",
-      //   status: "available",
-      // },
-      // {
       //   name: "MXNE",
       //   mintAddress: "6zYgzrT7X2wi9a9NeMtUvUWLLmf2a8vBsbYkocYdB9wa",
       //   decimals: 9,
       //   image: "/assets/svg/stablecoins/mxne.svg",
       //   status: "available",
       // },
-      {
-        name: "NGNC",
-        mintAddress: "BwaXCdG2hsW2PTieBc14Ys92yKVGWSQSATY9juwkBEZo",
-        decimals: 9,
-        image: "/assets/svg/stablecoins/ngnc.svg",
-        status: "unavailable",
-      },
+      // {
+      //   name: "NGNC",
+      //   mintAddress: "BwaXCdG2hsW2PTieBc14Ys92yKVGWSQSATY9juwkBEZo",
+      //   decimals: 9,
+      //   image: "/assets/svg/stablecoins/ngnc.svg",
+      //   status: "unavailable",
+      // },
       {
         name: "PYUSD",
         mintAddress: "2b1kV6DkPAnxd5ixfnxCpjxmKwqjjaYmCZfHsFu24GXo",
@@ -153,13 +139,13 @@ export const Tokens = [
         image: "/assets/svg/stablecoins/qcad.svg",
         status: "available",
       },
-      {
-        name: "TRYB",
-        mintAddress: "6ry4WBDvAwAnrYJVv6MCog4J8zx6S3cPgSqnTsDZ73AR",
-        decimals: 6,
-        image: "/assets/svg/stablecoins/tryb.svg",
-        status: "unavailable",
-      },
+      // {
+      //   name: "TRYB",
+      //   mintAddress: "6ry4WBDvAwAnrYJVv6MCog4J8zx6S3cPgSqnTsDZ73AR",
+      //   decimals: 6,
+      //   image: "/assets/svg/stablecoins/tryb.svg",
+      //   status: "unavailable",
+      // },
       {
         name: "USDC",
         mintAddress: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
@@ -174,13 +160,6 @@ export const Tokens = [
         image: "/assets/svg/stablecoins/usdt.svg",
         status: "available",
       },
-      // {
-      //   name: "USDY",
-      //   mintAddress: "A1KLoBrKBde8Ty9qtNQUtq3C2ortoC3u7twggz7sEto6",
-      //   decimals: 6,
-      //   image: "/assets/svg/stablecoins/usdy.svg",
-      //   status: "available",
-      // },
       // {
       //   name: "VCHF",
       //   mintAddress: "AhhdRu5YZdjVkKR3wbnUDaymVQL2ucjMQ63sZ3LFHsch",
