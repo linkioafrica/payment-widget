7672c893a42ab945df6cc7c153a7b4a8a0384673
tonygarhager <tony.garhager@gmail.com>
2025-05-15 18:10:39 +0200

    fix TRYB decimal

--- constants/token.ts
+++ constants/token.ts
@@ -296,7 +296,7 @@ export const Tokens = [
       {
         name: "TRYB",
         mintAddress: "0x18bc5bcc660cf2b9ce3cd51a404afe1a0cbd3c22",
-        decimals: 2,
+        decimals: 6,
         image: "/assets/svg/stables/tryb.svg",
         status: "available",
       },
